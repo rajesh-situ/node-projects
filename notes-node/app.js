50203e60c6ca6312ffd904fafdc807346fb9702f
Rajesh Panda <rajesh.situ@gmail.com>
2018-07-31 13:31:35 +0530

    npm module require

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,10 +2,14 @@ console.log('Starting app.js ....')
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js')
 
-var result = notes.add(3, 4);
-console.log(result);
+console.log(_.isString('Hello'));
+console.log(_.isString(1));
+console.log(_.uniq([1, 1, 2, 3, 7]));
+// var result = notes.add(3, 4);
+// console.log(result);
 // var user = os.userInfo()
 // fs.appendFile('greetings.txt', `Hello ${user.username}!. You are ${notes.age}.`, (error) => {
 
